e3fb53b83edf81e12fd10822a8bdad5ed8202711
Arvey Jimenez <arve505@gmail.com>
2024-01-31 10:15:32 +0800

    initial server setup.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('test');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3100;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
